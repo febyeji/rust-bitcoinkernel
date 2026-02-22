b9a70177c7fe129d86d56b61335ca5c9e6b32d2c
Yeji Han <yeji.han@sf.snu.ac.kr>
2026-02-22 17:02:13 +0900

    script_debug: add C++ EvalScript hook with fExec

---
+++ libbitcoinkernel-sys/bitcoin/src/script/debug.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2024-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <script/debug.h>
+
+#include <span>
+#include <vector>
+
+DebugScriptCallback g_script_debug_callback{nullptr};
+
+void DebugScript(std::span<const std::vector<unsigned char>> stack, const CScript& script, uint32_t opcode_pos, std::span<const std::vector<unsigned char>> altstack, bool fExec)
+{
+    if (g_script_debug_callback) g_script_debug_callback(stack, script, opcode_pos, altstack, fExec);
+}
+
+void RegisterDebugScriptCallback(DebugScriptCallback func)
+{
+    g_script_debug_callback = func;
+}
