a0a4d0ef10b803d418a900c3b56488e470a57619
Yeji Han <yeji.han@sf.snu.ac.kr>
2026-02-24 15:56:21 +0900

    script_debug: add mutex on script_debug_callback

--- libbitcoinkernel-sys/bitcoin/src/script/debug.cpp
+++ libbitcoinkernel-sys/bitcoin/src/script/debug.cpp
@@ -4,17 +4,21 @@
 
 #include <script/debug.h>
 
+#include <mutex>
 #include <span>
 #include <vector>
 
-DebugScriptCallback g_script_debug_callback{nullptr};
+static std::mutex g_script_debug_mutex;
+static DebugScriptCallback g_script_debug_callback{nullptr};
 
 void DebugScript(std::span<const std::vector<unsigned char>> stack, const CScript& script, uint32_t opcode_pos, std::span<const std::vector<unsigned char>> altstack, bool fExec)
 {
+    std::lock_guard<std::mutex> lock(g_script_debug_mutex);
     if (g_script_debug_callback) g_script_debug_callback(stack, script, opcode_pos, altstack, fExec);
 }
 
 void RegisterDebugScriptCallback(DebugScriptCallback func)
 {
+    std::lock_guard<std::mutex> lock(g_script_debug_mutex);
     g_script_debug_callback = func;
 }
